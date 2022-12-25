569804d84565cef233ece0561386cdfc6e0e07d2
sharmakavya281105 <kavyarahul9610@gmail.com>
2022-12-25 18:42:44 +0530

    book repair module done ready for testing

--- pages/book-a-repair/address/index.js
+++ pages/book-a-repair/address/index.js
@@ -4,62 +4,92 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { useGetAddressQuery } from '../../../app/store/apiSlice'
+import { useDeleteAddressMutation, useGetAddressQuery, useLazyGetAddressQuery } from '../../../app/store/apiSlice'
 import { setAddressId } from '../../../app/store/repairOrederSlice'
 import BookRepairLayout from '../../../components/Layout/BookRepairLayout'
+
+
+const useAddress = () => {
+
+    const router = useRouter()
+    const dispatch = useDispatch()
+    const [get, { data, error, isLoading }] = useLazyGetAddressQuery()
+    const [deleteAddress, { data: deleted, error: deletetionError, isLoading: deleting }] = useDeleteAddressMutation()
+    useEffect(() => {
+        get()
+    }, [])
+    useEffect(() => {
+        console.log(data);
+        if (error?.status == 404) router.replace('address/add')
+    }, [error])
+    useEffect(() => {
+        if (deleted) {
+            get()
+        }
+    }, [deleted])
+    
+
+    return [deleteAddress, { data, isLoading, deleting }]
+}
 
 export default function Address() {
     const router = useRouter()
     const dispatch = useDispatch()
-    const { data, error, isLoading } = useGetAddressQuery()
-    
-    const [selected,setSelected]=useState()
-    useEffect(()=>{
-        console.log(data);
-        if(error?.status == 404) router.replace('address/add')
-        if(data instanceof Array && data[0]?._id) setSelected(data[0]._id)
-    },[data,error])
+    const [deleteAddress, { data, isLoading, deleting }] = useAddress()
+    useEffect(() => {
+        if (data instanceof Array && data[0]?._id) setSelected(data[0]._id)
 
-    const handleContinue = ()=>{
-        dispatch(setAddressId(selected))  
-        router.push('/book-a-repair/review')  
+    }, [data])
+    // const { data, error, isLoading } = useGetAddressQuery()
+    // const [deleteAddress, { data: deleted, error: deletetionError, isLoading: deleting }] = useDeleteAddressMutation()
+    // useEffect(() => {
+    //     console.log(data);
+    //     if (error?.status == 404) router.replace('address/add')
+    //     if (data instanceof Array && data[0]?._id) setSelected(data[0]._id)
+    // }, [data, error])
+    const [selected, setSelected] = useState()
+
+
+    const handleContinue = () => {
+        dispatch(setAddressId(selected))
+        router.push('/book-a-repair/review')
     }
 
     return (
         <>
             <h2 className='font-light text-4xl text-darkLight mb-8 cursor-pointer'>Book a repair</h2>
-            <div href='/book-a-repair/address/get' className='flex items-center mb-6' onClick={()=>router.push('/book-a-repair/address/add')}>
+            <div href='/book-a-repair/address/get' className='flex items-center mb-6' onClick={() => router.push('/book-a-repair/address/add')}>
                 <img src='/plus.webp' alt="" className='w-12' />
                 <p>Add new address</p>
             </div>
             {
                 data?.map(({
-                        _id,
-                        addressType,
-                        phoneNumber,
-                        area,
-                        plotNumber,
-                        landmark,
-                        city,
-                        state,
-                        pincode
+                    _id,
+                    addressType,
+                    phoneNumber,
+                    area,
+                    plotNumber,
+                    landmark,
+                    city,
+                    state,
+                    pincode
 
-                }) => <div className="mb-6 white-glass p-4 sm:p-8 rounded-xl" key={_id} onClick={()=>{setSelected(_id)} }>
-                    <div className="flex flex-col sm:flex-row">
-                        <div className="left">
-                            <div className="flex items-center mb-2">
-                                <input type="radio" name="" id="" checked={selected==_id} />
-                                <h3 className='ml-2 text-primary font-medium text-xl'>{addressType} Address</h3>
+                }) => <div className="mb-6 white-glass p-4 sm:p-8 rounded-xl" key={_id} onClick={() => { setSelected(_id) }}>
+                        <div className="flex flex-col sm:flex-row">
+                            <div className="left">
+                                <div className="flex items-center mb-2">
+                                    <input type="radio" name="" id="" checked={selected == _id} />
+                                    <h3 className='ml-2 text-primary font-medium text-xl'>{addressType} Address</h3>
+                                </div>
+                                <p className='text-gray-500 mb-2'>{`${plotNumber}, ${area} ,${city}, ${state} - ${pincode}`}</p>
+                                <p className='text-gray-500 mb-2'><b>Mobile: </b>{phoneNumber}</p>
                             </div>
-                            <p className='text-gray-500 mb-2'>{`${plotNumber}, ${area} ,${city}, ${state} - ${pincode}`}</p>
-                            <p className='text-gray-500 mb-2'><b>Mobile: </b>{phoneNumber}</p>
+                            <div className="right sm:w-28 w-full flex justify-end sm:block">
+                                <img src="/options.svg" alt="" className='w-12 h-12 sm:mb-4 mr-4' />
+                                <img src="/trash.svg" alt="" className='w-14 h-14 sm:mb-4 mr-4' onClick={() => { deleteAddress(_id) }} />
+                            </div>
                         </div>
-                        <div className="right sm:w-28 w-full flex justify-end sm:block">
-                            <img src="/options.svg" alt="" className='w-12 h-12 sm:mb-4 mr-4' />
-                            <img src="/trash.svg" alt="" className='w-14 h-14 sm:mb-4 mr-4' />
-                        </div>
-                    </div>
-                </div>)
+                    </div>)
 
             }
             <button className='brand-btn' onClick={handleContinue}>
